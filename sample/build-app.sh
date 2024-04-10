b7ee2da10646db6065efd317460b062fbe61dba1
sun <asmh1989@gmail.com>
2024-04-10 10:44:47 +0800

    support choose qtversion

--- sample/build-app.sh
+++ sample/build-app.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd $1
 git pull origin master
-docker run -v $HOME/raspi:/root/raspi asmh1989/build-pi-ubuntu bash /root/raspi/sample/qt.sh $1
+docker run -v $HOME/raspi:/root/raspi asmh1989/build-pi-ubuntu bash /root/raspi/sample/qt.sh $1 $2
--- sample/qt.sh
+++ sample/qt.sh
@@ -3,6 +3,13 @@
 cd $HOME/raspi/sample/$1
 mkdir -p build
 cd build
+
+version=$2
+if [ -z "$version" ];then
+    version=6.6.2
+fi
+
+echo "use version = $version  2 = $2"
 # ~/raspi/qt-pi-64/6.5.3/bin/qt-cmake ..
 ~/raspi/qt-pi-64/6.6.2/bin/qt-cmake .. #-DCMAKE_BUILD_TYPE=Debug 
 cmake --build . --parallel 8
